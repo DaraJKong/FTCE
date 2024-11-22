0125aa7e6237394202a20126d2d257b61a07e1da
DaraJKong <daraetkong@gmail.com>
2024-11-22 14:05:05 -0500

    Add functionality to ClearData

--- src/lib/ClearData.jsx
+++ src/lib/ClearData.jsx
@@ -1,8 +1,13 @@
 import { ConfirmationModal } from "/src/lib/ui/Modal";
 
 export default function ClearData() {
+  const clearData = () => {
+    chrome.storage.sync.clear();
+    chrome.storage.local.clear();
+  };
+
   return (
-    <ConfirmationModal message="Clear Data" onConfirm={() => {}} onCancel={() => {}}>
+    <ConfirmationModal message="Clear Data" onConfirm={clearData}>
       <h3 class="text-xl font-bold">Are you sure?</h3>
       <p class="py-4">
         You are about to delete all the data linked to this Chrome extension. This includes any synced data related to
